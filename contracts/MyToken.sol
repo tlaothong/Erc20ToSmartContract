696bd1d8111262fc51157ac458987329ce680d9b
Teerachai Laothong <tlaothong@gmail.com>
2022-06-25 14:46:47 +0700

    Added withdraw to bridge out to eth

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -5,11 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
 
+  address _owner;
+
   constructor() ERC20("MyToken", "MTK") {
     _mint(msg.sender, 200 * 10 ** decimals());
+    _owner = msg.sender;
   }
 
   function bridge() payable public {
-    _mint( msg.sender, msg.value * 10);
+    _transfer(_owner, msg.sender, msg.value * 10);
+  }
+
+  function withdraw(uint amount) public {
+    _transfer(msg.sender, _owner, amount * 10);
+    payable(msg.sender).transfer(amount);
   }
 }
