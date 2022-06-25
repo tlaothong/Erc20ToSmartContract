82b22d6958d76765d67b046b85c66728a88a2e33
Teerachai Laothong <tlaothong@gmail.com>
2022-06-25 13:36:02 +0700

    Added bridge example

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 200 * 10 ** decimals());
-    }
+
+  constructor() ERC20("MyToken", "MTK") {
+    _mint(msg.sender, 200 * 10 ** decimals());
+  }
+
+  function bridge() payable public {
+    _mint( msg.sender, msg.value * 10);
+  }
 }
